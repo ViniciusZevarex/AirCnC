0b3a73ed4c47f5a8067ade45bd00991351d311f1
ViniciusZevarex <vinicius.zevarex2002@gmail.com>
2019-10-03 13:23:19 -0300

    Primeiro commit com o back-end(NODEJS) e o front-end(REACTJS), próxima etapa é a construção do Mobile(REACT NATIVE)

---
+++ backend/src/server.js
@@ -0,0 +1,32 @@
+const express 	= require('express');
+const mongoose	= require('mongoose');
+const cors		= require('cors')
+const routes 	= require('./routes');
+const path		= require('path'); 
+
+
+//express é um microframework para não precisar escrever coisas que já estão prontas
+const app = express();
+
+mongoose.connect('mongodb+srv://omnisctack:123123123@cluster0-spl4w.mongodb.net/test?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+//pegar requisição e enviar resposta
+
+//req.query = acessar query params (para filtros)
+//req.params = acessar route params (para edição, delete)
+//req.body =  acessar corpo da requisição (para criação, edição)
+
+app.use(cors());
+app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname,'..','uploads')));
+app.use(routes);
+
+//GET, POST, PUT, DELETE
+// app.post('/user', (req,res) =>{
+// 	return res.json({ message: req.body});
+// });
+
+//porta do localhost onde a aplicação será executada
+app.listen(4444);
